2d1b1f310ac54d2b48dbeff3e921029a2c1eb3b6
PsIke25 <bestpahpsike@gmail.com>
2025-05-26 21:27:08 -0300

    Delete tcc directory

--- tcc/FRONT/TCC_BestPath/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: 'http://localhost:2024'
-})
-
-export default api
--- tcc/FRONT/TCC_BestPath/src/components/button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./Button.module.css"
-
-const Button = ({type, onClick, className, text}) => {
-    return(
-        <button type={type} onClick={onClick} className={className}>{text}</button>
-    )
-}
-
-export default Button
